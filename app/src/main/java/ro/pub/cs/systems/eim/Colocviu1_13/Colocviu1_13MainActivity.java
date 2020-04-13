77b3d34ee081f84273109794641b4ba8c10a4269
BogdanR97 <romanbogdan97@gmail.com>
2020-04-13 11:41:46 +0300

    C

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -1,13 +1,16 @@
 package ro.pub.cs.systems.eim.Colocviu1_13;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Colocviu1_13MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,8 @@ public class Colocviu1_13MainActivity extends AppCompatActivity {
         Button east = findViewById(R.id.eastBtn);
         Button west = findViewById(R.id.westBtn);
         final EditText cardinal = findViewById(R.id.cardinalText);
+
+        Button navigator = findViewById(R.id.navigateBtn);
 
         north.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,11 +65,35 @@ public class Colocviu1_13MainActivity extends AppCompatActivity {
                 pressedNo++;
             }
         });
+
+        navigator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Colocviu1_13MainActivity.this, Colocviu1_13SecondaryActivity.class);
+                intent.putExtra("CARDINAL", cardinal.getText().toString());
+
+                cardinal.setText("");
+                pressedNo = 0;
+
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("PRESSED_NO", pressedNo);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == 0)
+            Toast.makeText(this, "REGISTER was pressed", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(this, "CANCEL was pressed", Toast.LENGTH_LONG).show();
+
     }
 }
---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13SecondaryActivity.java
@@ -0,0 +1,46 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Colocviu1_13SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1_colocviu1_13_secondary);
+
+        Intent intent = getIntent();
+        String cardinalStr = intent.getStringExtra("CARDINAL");
+
+        EditText cardinalView = findViewById(R.id.cardinalSecText);
+        if (cardinalStr != null)
+            cardinalView.setText(cardinalStr);
+
+        Button register = findViewById(R.id.registerBtn);
+        Button cancel = findViewById(R.id.cancelBtn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(0);
+                finish();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(1);
+                finish();
+            }
+        });
+
+    }
+}
